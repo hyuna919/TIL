f7d53f359b1d89d81a53ef98d2d0fc1003d93ab2
hyuna919 <hyuna919@naver.com>
2022-12-13 17:28:26 +0900

    Feature 카메라,오디오 불러오기+제어버튼

--- websocket/nomadZoomClone/src/public/js/app.js
+++ websocket/nomadZoomClone/src/public/js/app.js
@@ -5,3 +5,53 @@ WebRTC 버전 front 코드
 */
 
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const cameraBtn = document.getElementById("cameraBtn");
+const micBtn = document.getElementById("micBtn");
+
+let myStream;
+let cameraState = false;
+let micState = false;
+
+getMedia();
+
+cameraBtn.addEventListener("click", handleCamereClick);
+micBtn.addEventListener("click", handleMicClick);
+
+/*
+함수영역
+*/
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: micState,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function handleCamereClick() {
+  if (!cameraState) {
+    cameraBtn.innerText = "UNMUTE";
+    cameraState = true;
+  } else {
+    cameraBtn.innerText = "Mute";
+    cameraState = false;
+  }
+  getMedia();
+}
+
+function handleMicClick() {
+  if (!micState) {
+    micBtn.innerText = "UNMUTE";
+    micState = true;
+  } else {
+    micBtn.innerText = "Mute";
+    micState = false;
+  }
+  getMedia();
+}
--- websocket/nomadZoomClone/src/server.js
+++ websocket/nomadZoomClone/src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import express from "express";
-import { WebSocketServer } from "ws";
+import WebSocket from "ws";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app); // express를 http서버로 만들어야 접근할 수 있어서 이렇게 함.
-const wss = new WebSocket.Server({ server }); // 인자로 server를 넘셔서 같은 포트에서 http, ws모두 사용할 수 있게됨
+const wss = SocketIO(httpServer); // 인자로 server를 넘셔서 같은 포트에서 http, ws모두 사용할 수 있게됨
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
