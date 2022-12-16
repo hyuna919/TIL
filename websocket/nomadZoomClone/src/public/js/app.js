d6b8901987bf0a2ac00d8f19443a95f03503efc3
hyuna919 <hyuna919@naver.com>
2022-12-16 17:05:20 +0900

    Feature mediaDevices를 이용해 장치정보 가져오기

--- websocket/nomadZoomClone/src/public/js/app.js
+++ websocket/nomadZoomClone/src/public/js/app.js
@@ -9,15 +9,18 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const cameraBtn = document.getElementById("cameraBtn");
 const micBtn = document.getElementById("micBtn");
+const cameraSelect = document.getElementById("cameraSelect");
 
 let myStream;
 let cameraState = false;
 let micState = false;
 
-getMedia();
-
+// 리스너
 cameraBtn.addEventListener("click", handleCamereClick);
 micBtn.addEventListener("click", handleMicClick);
+
+// 미디어 불러오기
+getMedia();
 
 /*
 함수영역
@@ -25,33 +28,57 @@ micBtn.addEventListener("click", handleMicClick);
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: micState,
+      audio: true,
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
 }
 
 function handleCamereClick() {
+  console.log(myStream.getVideoTracks());
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!cameraState) {
-    cameraBtn.innerText = "UNMUTE";
+    cameraBtn.innerText = "🎥 TURN ON";
     cameraState = true;
   } else {
-    cameraBtn.innerText = "Mute";
+    cameraBtn.innerText = "🎥 TURN OFF";
     cameraState = false;
   }
-  getMedia();
 }
 
 function handleMicClick() {
+  console.log(myStream.getAudioTracks());
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+
   if (!micState) {
-    micBtn.innerText = "UNMUTE";
+    micBtn.innerText = "🎤 UNMUTE";
     micState = true;
   } else {
-    micBtn.innerText = "Mute";
+    micBtn.innerText = "🎤 MUTE";
     micState = false;
   }
-  getMedia();
+}
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    console.log(cameras);
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
